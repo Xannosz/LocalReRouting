bf7153ae2ca6503be4aaa758b88aa2b185bee594
xannosz <joli.neni@freemail.hu>
2020-04-09 18:39:43 +0200

    Working layout

--- src/main/java/hu/xannosz/local/rerouting/core/App.java
+++ src/main/java/hu/xannosz/local/rerouting/core/App.java
@@ -1,4 +1,48 @@
 package hu.xannosz.local.rerouting.core;
 
-public class App {
+import hu.xannosz.local.rerouting.core.graph.CreateSpecialGraph;
+import org.graphstream.graph.Graph;
+import org.graphstream.ui.view.Viewer;
+import org.graphstream.ui.view.ViewerListener;
+import org.graphstream.ui.view.ViewerPipe;
+
+public class App implements ViewerListener {
+    protected boolean loop = true;
+
+    public static void main(String args[]) {
+        new App();
+    }
+
+    public App() {
+        Graph graph = CreateSpecialGraph.createCompleteGraph("Complete",10);
+        //Graph graph = CreateSpecialGraph.createPairGraph("Pair",10,10);
+        //Graph graph = CreateSpecialGraph.createPairGraph("Pair",10,5);
+        //Graph graph = CreateSpecialGraph.createPetersenGraph("Petersen");
+        Viewer viewer = graph.display();
+        viewer.disableAutoLayout();
+
+        viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.EXIT);
+
+        ViewerPipe fromViewer = viewer.newViewerPipe();
+        fromViewer.addViewerListener(this);
+        fromViewer.addSink(graph);
+
+        while (loop) {
+            fromViewer.pump();
+
+            // here your simulation code.
+        }
+    }
+
+    public void viewClosed(String id) {
+        loop = false;
+    }
+
+    public void buttonPushed(String id) {
+        System.out.println("Button pushed on node " + id);
+    }
+
+    public void buttonReleased(String id) {
+        System.out.println("Button released on node " + id);
+    }
 }
